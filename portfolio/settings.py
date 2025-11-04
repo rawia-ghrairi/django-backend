005c0c388c324094e8af21f27d7d52ab928637dc
rawia-ghrairi <rawiaghrairi@gmail.com>
2025-11-04 21:42:35 +0100

    last commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -148,8 +148,9 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 CORS_ALLOWED_ORIGINS = [
    "http://localhost:5173",
-   "https://my-portfolio-gamma-eight-43.vercel.app/"
+   "https://my-portfolio-gamma-eight-43.vercel.app"
 ]
+
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://.*\.vercel\.app$",
 ]
